63f3c5915dcb0c51be2a37aee322158eceedda3c
Alejandro Ravasio <alejandroravasio@gmail.com>
2019-06-06 08:04:34 -0300

    Initial commit. Cocoapods support added.

---
+++ WhiteSpectreDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhiteSpectreDemo
+//
+//  Created by Alejandro Ravasio on 06/06/2019.
+//  Copyright © 2019 Alejandro Ravasio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
